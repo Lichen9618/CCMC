6e4600feab0fed85b6495f6ee5a99de0a79ecc97
Shuai <bfshm@qq.com>
2019-09-11 18:10:21 +0800

    upate

---
+++ CrossChainContract/CrossChainContract/Contract1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace CrossChainContract
+{
+    class Contract1 : SmartContract
+    {
+        public static bool Main()
+        {
+            Storage.Put("Hello", "World");
+            return true;
+        }
+    }
+}
---
+++ CrossChainContract/SyncHeaderContract/Contract1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace SyncHeaderContract
+{
+    class Contract1 : SmartContract
+    {
+        public static bool Main(string method,object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (method == "SyncBlockHeader")
+                {
+                    return SyncHeader((byte[]) args[0]);
+                }
+            }
+
+            return true;
+        }
+
+        [Syscall("Neo.CrossChain.SyncBlockHeader")]
+        public static extern bool SyncHeader(byte[] paraBytes);
+    }
+}
