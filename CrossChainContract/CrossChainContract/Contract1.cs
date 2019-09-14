88b46429e0996064641c3b91ed0d19fdb4b0233b
晨 黎 <lichen@neo.org>
2019-09-14 18:31:02 +0800

    Add processCrossChainTx

--- CrossChainContract/CrossChainContract/Contract1.cs
+++ CrossChainContract/CrossChainContract/Contract1.cs
@@ -4,12 +4,20 @@ using Neo.SmartContract.Framework.Services.Neo;
 
 namespace CrossChainContract
 {
-    class Contract1 : SmartContract
+    class Contract : SmartContract
     {
-        public static bool Main()
-        {
-            Storage.Put("Hello", "World");
+        delegate object DynCall(string method, object[] args);
+
+        public static bool Main(string method, object[] args)
+        {            
             return true;
+        }
+
+        private static object ProcessCrossChainTx(byte[] Target, string operation, object[] args)
+        {
+            if (Target.Length != 20) return false;
+            DynCall TargetContract = (DynCall)Target.ToDelegate();
+            return TargetContract(operation, args);
         }
     }
 }
