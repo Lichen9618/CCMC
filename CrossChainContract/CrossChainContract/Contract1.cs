b8f07f894f766590df673a9b096042559f0aa642
Shuai <bfshm@qq.com>
2019-09-16 11:22:02 +0800

    合约

--- CrossChainContract/CrossChainContract/Contract1.cs
+++ CrossChainContract/CrossChainContract/Contract1.cs
@@ -6,10 +6,20 @@ namespace CrossChainContract
 {
     class Contract1 : SmartContract
     {
-        public static bool Main()
+        public static bool Main(string method, object[] args)
         {
-            Storage.Put("Hello", "World");
-            return true;
+            if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (method == "CreateCrossChainTx")
+                {
+                    return CreateCrossChainTransaction((byte[])args[0]);
+                }
+            }
+
+            return false;
         }
+
+        [Syscall("Neo.CrossChain.CreateTransaction")]
+        public static extern bool CreateCrossChainTransaction(byte[] paraBytes);
     }
 }
--- CrossChainContract/SyncHeaderContract/Contract1.cs
+++ CrossChainContract/SyncHeaderContract/Contract1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
 
 namespace SyncHeaderContract
 {
     class Contract1 : SmartContract
     {
-        public static bool Main(string method,object[] args)
+        public static object Main(string method,object[] args)
         {
             if (Runtime.Trigger == TriggerType.Application)
             {
@@ -14,12 +15,37 @@ namespace SyncHeaderContract
                 {
                     return SyncHeader((byte[]) args[0]);
                 }
+
+                if (method == "GetCurrentHeight")
+                {
+                    //获取本合约地址hash
+                    var contract = ExecutionEngine.ExecutingScriptHash;
+                    var chainIDBytes = (byte[])args[0];
+                    Runtime.Log("chain id is:"+ ToIntString(chainIDBytes));
+                    var key = contract.Concat("CURRENT_HEIGHT".AsByteArray()).Concat(chainIDBytes);
+                    Runtime.Log("chain height key is:"+ToHexString(key));
+                    return Storage.Get(key).AsBigInteger();
+                }
+                if (method == "GetBlockHeaderHash")
+                {
+                    //获取本合约地址hash
+                    var contract = ExecutionEngine.ExecutingScriptHash;
+                    var chainIDBytes = (byte[])args[0];
+                    var heightBytes = (byte[]) args[1];
+                    var key = "HEADER_INDEX".AsByteArray().Concat(chainIDBytes).Concat(heightBytes);
+                    return Storage.Get(key);
+                }
             }
 
-            return true;
+            return false;
         }
 
         [Syscall("Neo.CrossChain.SyncBlockHeader")]
         public static extern bool SyncHeader(byte[] paraBytes);
+
+        [Syscall("Neo.CrossChain.IntToString")]
+        public static extern string ToIntString(byte[] paraBytes);
+        [Syscall("Neo.CrossChain.ToHexString")]
+        public static extern string ToHexString(byte[] paraBytes);
     }
 }
